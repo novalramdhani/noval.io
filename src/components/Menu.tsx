22be098bb393e26ae3f823626758284d06c36e19
novalramdhani <novalfitrahramdhani@gmail.com>
2022-02-23 01:31:47 +0700

    add project card

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function Menu() {
     return (
-        <p className="mt-5">
+        <p className="mt-5 mb-5">
             <a href="" className="text-sky-400 underline"><Link to="/">Home</Link></a>
             <a href="" className="text-sky-400 underline ml-5"><Link to="/projects">Projects</Link></a>
         </p>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,17 @@
 import Container from "./Container";
+import Card from "./Card";
+import Menu from "./Menu";
 
 export default function Projects() {
     return (
         <Container>
-            <h1 className="text-white font-bold uppercase">All my projects</h1>
+            <h1 className="text-white font-bold uppercase mt-5">All my projects</h1>
+            <Card link="http://movie-cast.herokuapp.com" title="Movie Cast" description="A movie website created using Laravel and for styling using TailwindCSS, the api is taken from the TMDB API." />
+            <Card title="LaraBlog" description="A blog website using Laravel, PHP, MySQL and for styling using Bootstrap. Equipped with features such as authentication, CRUD." />
+            <Card title="Miolica" description="An e-commerce website uses Laravel API, Sanctum API Token, PHP, GraphQL, PostgreSQL as the backend and React Typescript, Redux, TailwindCSS as the frontend. Until implementing PWA (Progressive Web Apps)." />
+            <Card title="Ani Cast" description="A anime shows website created using Laravel and for styling using TailwindCSS, the api is taken from the Jikan API." />
+            <Card title="Social Network" description="A simple social media website using Laravel, MySQL and TailwindCSS. Features Authentication, Account Settings, Following, Follwers, Unfollowing, CRUD tweets, Likes, Unlike Tweets and Comments." />
+            <Menu />
         </Container>
     );
 }
