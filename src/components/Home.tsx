342568aa30fdc7201706f9653b2d7ec631091b25
novalramdhani <novalfitrahramdhani@gmail.com>
2022-03-18 17:22:02 +0700

    refactoring

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,10 +11,12 @@ export default function Home() {
             <div>
               <img src={me} className="shadows shadow-purple-400/20 bg-contain mx-auto p-1 rounded-full" alt="The picture of me" loading="lazy" width="100" height="100" />
             </div>
-            <div className="leading-loose text-lg">
-                  Hi 👋, I'm Noval Ramdhani. Web programming enthusiast, Big fan of <SocialMediaLink url={url.frameworks.laravel} name="Laravel" />, <SocialMediaLink url={url.frameworks.rails} name="Rails" /> and <SocialMediaLink url={url.frameworks.react} name="React" />.
+            <div className="leading-loose">
                 <p className="mt-4">
-                  I have social media like <SocialMediaLink url={url.instagram} name="Instagram" />, <SocialMediaLink url={url.twitter} name="Twitter" /> or <SocialMediaLink url={url.showwcase} name="Showwcase" />. If you want to know my activities on the internet or you can also follow my daily writing articles on <SocialMediaLink url={url.devto} name="Dev.to" />.
+                  Hi 👋, I'm Noval Ramdhani. Web programming enthusiast, Big fan of <SocialMediaLink url={url.frameworks.laravel} name="Laravel" />, <SocialMediaLink url={url.frameworks.rails} name="Rails" /> and <SocialMediaLink url={url.frameworks.react} name="React" />.
+                </p>
+                <p className="mt-4">
+                  I have social media like <SocialMediaLink url={url.instagram} name="Instagram" /> and <SocialMediaLink url={url.twitter} name="Twitter" /> If you want to know my activities on the internet or you can also follow my daily writing articles on <SocialMediaLink url={url.devto} name="DEV" />.
                 </p>
                 <p className="mt-4">
                   You can find all my programming lessons on <SocialMediaLink url={url.github} name="Github" />.
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 export default function Menu() {
     return (
         <div className="mt-5 mb-5">
-            <a href="" className="text-sky-400 underline"><Link to="/">Home</Link></a>
-            <a href="" className="text-sky-400 underline ml-5"><Link to="/projects">Projects</Link></a>
+            <span className="text-sky-400"><Link to="/">Home</Link></span>
+            <span className="text-sky-400 ml-5"><Link to="/projects">Projects</Link></span>
         </div>
     )
 }
