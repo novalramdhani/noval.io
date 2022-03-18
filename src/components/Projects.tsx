3ccc294b93727408d43d5c27ba382822d0f32d58
novalramdhani <novalfitrahramdhani@gmail.com>
2022-03-18 17:44:10 +0700

    add projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,8 @@ export default function Projects() {
     return (
         <Container>
             <h1 className="text-white font-bold text-lg uppercase mt-5">All my projects</h1>
-            <Card title="Movie Cast" description="Movie Tv Shows Web App" />
+            <Card title="Movie Cast" description="Movie Tv Shows Web App" sourceCodeUrl="https://github.com/novalramdhani/Movie-Cast" previewUrl="http://movie-cast.herokuapp.com" />
+            <Card title="Ani Cast" description="Anime Shows Web App" sourceCodeUrl="https://github.com/novalramdhani/Ani-Cast" previewUrl="http://anicast.herokuapp.com" />
             <Menu />
         </Container>
     );
