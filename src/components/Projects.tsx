2de84e82acf964fb3c38bc35fd0f31adcb02d669
novalramdhani <novalfitrahramdhani@gmail.com>
2022-03-18 17:52:48 +0700

    wip

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,7 +5,7 @@ import Menu from "./Menu";
 export default function Projects() {
     return (
         <Container>
-            <h1 className="text-white font-bold text-lg uppercase mt-5">All my projects</h1>
+            <h1 className="text-white font-bold text-lg uppercase mt-5">My projects</h1>
             <Card title="Movie Cast" description="Movie Tv Shows Web App" sourceCodeUrl="https://github.com/novalramdhani/Movie-Cast" previewUrl="http://movie-cast.herokuapp.com" />
             <Card title="Ani Cast" description="Anime Shows Web App" sourceCodeUrl="https://github.com/novalramdhani/Ani-Cast" previewUrl="http://anicast.herokuapp.com" />
             <Menu />
