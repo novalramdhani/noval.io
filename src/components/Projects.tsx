679f96b0d2aff7f99a8ba76ebba109ce97abc3b0
novalramdhani <novalfitrahramdhani@gmail.com>
2022-04-20 22:58:22 +0700

    fix

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,25 +10,30 @@ export default function Projects() {
             <div className="w-full flex flex-wrap flex-col sm:flex-row justify-center items-center gap-4 mt-5">
                 <Card
                     title="Movie Cast"
-                    description="Movie Tv Shows Web App"
+                    description="Ani Cast is a simple movie shows application using a TALL stack with the integration of the tmdb.org API."
                     sourceCodeUrl="https://github.com/novalramdhani/Movie-Cast"
                     previewUrl="http://movie-cast.herokuapp.com"
                 />
                 <Card
                     title="Ani Cast"
-                    description="Anime Shows Web App"
+                    description="Ani Cast is a simple anime movie application using a TALL stack with the integration of the jikan API."
                     sourceCodeUrl="https://github.com/novalramdhani/Ani-Cast"
                     previewUrl="http://anicast.herokuapp.com"
                 />
                 <Card
                     title="Social Network"
-                    description="Social Media Web App"
+                    description="Social Network is a social media web app build using Laravel, PostgreSQL, Tailwind CSS with include the API."
                     sourceCodeUrl="https://github.com/novalramdhani/Social-Network"
                 />
                 <Card
                     title="Blog App"
-                    description="Blog Web App"
+                    description="A simple blog application using Laravel, PostgreSQL, Bootstrap CSS with feature CRUD, comment and more."
                     sourceCodeUrl="https://github.com/novalramdhani/blog"
+                />
+                <Card
+                    title="Miolica"
+                    description="Miolica is an simple e-commerce application that uses React.js as the frontend and Laravel as the backend."
+                    sourceCodeUrl="https://github.com/novalramdhani/Miolica"
                 />
             </div>
 
